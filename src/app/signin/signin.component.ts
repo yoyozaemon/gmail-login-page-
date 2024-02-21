0a615dc0626214de3f185e51541706aac1a64d21
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2024-02-21 17:05:23 +0530

    ...

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css',
+  imports: [MatButtonModule,MatFormFieldModule, MatInputModule],
+  })
 
-})
 export class SigninComponent {
   email: string = ' ';
 
